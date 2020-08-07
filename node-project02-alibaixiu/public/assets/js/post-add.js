24238b302d4b602f0a0d41a29faa904fd13f2ce7
a-ke <642028114@qq.com>
2020-08-07 09:58:28 +0800

    文章列表修改与删除

--- node-project02-alibaixiu/public/assets/js/post-add.js
+++ node-project02-alibaixiu/public/assets/js/post-add.js
@@ -1,13 +1,15 @@
-// 向服务器端发送请求 获取文章分类数据
-$.ajax({
-	url: '/categories',
-	type: 'get',
-	success: function (response) {
-		console.log(response)
-		var html = template('categoryTpl', {data: response});
-		$('#category').html(html);
+// 从浏览器的地址栏中获取查询参数
+function getUrlParams(name) {
+	var paramsAry = location.search.substr(1).split('&');
+	// 循环数据
+	for (var i = 0; i < paramsAry.length; i++) {
+		var tmp = paramsAry[i].split('=');
+		if (tmp[0] == name) {
+			return tmp[1];
+		}
 	}
-});
+	return -1;
+}
 
 // 当管理员选择文件的时候 触发事件
 $('#feature').on('change', function () {
@@ -48,5 +50,58 @@ $('#addForm').on('submit', function () {
 		}
 	})
 	// 阻止表单默认提交的行为
+	return false;
+});
+
+// 获取浏览器地址栏中的id参数
+var id = getUrlParams('id');
+// 当前管理员是在做修改文章操作
+if (id != -1) {
+	// 根据id获取文章的详细信息
+	$.ajax({
+		type: 'get',
+		url: '/posts/' + id,
+		success: function (response) {
+			$.ajax({
+				url: '/categories',
+				type: 'get',
+				success: function (categories) {
+					response.categories = categories;
+					console.log(response)
+					var html = template('modifyTpl', response);
+					$('#parentBox').html(html);
+				}
+			})
+			
+		}
+	})
+}else{
+	// 向服务器端发送请求 获取文章分类数据
+	$.ajax({
+		url: '/categories',
+		type: 'get',
+		success: function (response) {
+			console.log(response)
+			var html = template('categoryTpl', {data: response});
+			$('#category').html(html);
+		}
+	});
+}
+
+// 当修改文章信息表单发生提交行为的时候
+$('#parentBox').on('submit', '#modifyForm', function () {
+	// 获取管理员在表单中输入的内容
+	var formData = $(this).serialize()
+	// 获取管理员正在修改的文章id值
+	var id = $(this).attr('data-id');
+	$.ajax({
+		type: 'put',
+		url: '/posts/' + id,
+		data: formData,
+		success: function () {
+			location.href = '/admin/posts.html';
+		}
+	})
+	// 阻止表单默认提交行为
 	return false;
 });
--- node-project02-alibaixiu/routes/post.js
+++ node-project02-alibaixiu/routes/post.js
@@ -2,4 +2,19 @@ const post = require('express').Router();
 // 添加文章信息
 post.post('/', require('./actions/post/create'));
 
+// 查询所有文章
+post.get('/', require('./actions/post/find'));
+
+// 查询文章数量
+post.get('/count', require(('./actions/post/count')));
+
+// 根据文章id获取文章信息
+post.get('/:id', require('./actions/post/findById'));
+
+// 根据ID修改文章
+post.put('/:id', require('./actions/post/findByIdAndUpdate'));
+
+// 根据ID删除文章
+post.delete('/:id', require('./actions/post/findByIdAndDelete'));
+
 module.exports = post;
