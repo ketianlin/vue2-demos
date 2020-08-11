e2c0c93f99232dba2b22cc37ddb0537dd50d6ad7
a-ke <642028114@qq.com>
2020-08-11 19:12:51 +0800

    webpack打包demo01

---
+++ day05_webpack_demo/src/index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery'
+import './css/1.css'
+import './css/1.less'
+import './css/1.scss'
+
+
+$(function(){
+    $('li:odd').css('backgroundColor', 'blue');
+    $('li:even').css('backgroundColor', 'lightblue');
+})
