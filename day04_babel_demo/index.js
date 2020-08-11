a5f4228f81d58c680b5fe4e38c0239c81c9f29b6
a-ke <642028114@qq.com>
2020-08-11 14:15:19 +0800

    babel导入导出demo

---
+++ day04_babel_demo/index.js
@@ -0,0 +1,8 @@
+import m1,{f, s, say} from './m1.js'
+import './m2.js'
+
+console.log(m1)
+console.log(f)
+console.log(s)
+console.log(say())
+
---
+++ day04_babel_demo/m1.js
@@ -0,0 +1,20 @@
+let a = 10
+let c = 20
+let d = 30
+
+function show() {
+  console.log('1111111111111')
+}
+
+export function say(){
+    return 'say hello';
+}
+
+export let f = 55;
+export let s = 'hi';
+
+export default {
+  a,
+  c,
+  show
+}
